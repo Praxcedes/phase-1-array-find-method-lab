9341546c66b45783d0d6f1f323fad2e8f057bdac
Praxcedes <praxcedes.kabeya@example.com>
2025-07-02 21:05:33 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+const drivers = [
+  { name: "Bobby", hometown: "Pittsburgh" },
+  { name: "Sammy", hometown: "New York" },
+  { name: "Sally", hometown: "Cleveland" },
+  { name: "Annette", hometown: "Los Angeles" },
+  { name: "Bobby", hometown: "Tampa Bay" }
+];
+
+function findDriverByName(name) {
+  return drivers.find(driver => driver.name === name);
+}
+
+module.exports = {
+  findDriverByName,
+};
+// index.js
+
+function superbowlWin(record) {
+  const win = record.find(game => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+module.exports = {
+  superbowlWin,
+};
+
